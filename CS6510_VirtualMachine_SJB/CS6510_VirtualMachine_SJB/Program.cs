17a7320d7b07489c3f82d61a370c65e6cfc24a7e
Slizaaaaard <sjbush92@gmail.com>
2019-08-26 11:43:34 -0600

    first commit

---
+++ CS6510_VirtualMachine_SJB/CS6510_VirtualMachine_SJB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS6510_VirtualMachine_SJB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
