c77120e4dd8cb315e55be1c2559ad525a54096f5
Slizaaaaard <sjbush92@gmail.com>
2019-08-27 13:33:11 -0600

    Math

---
+++ CS6510_VirtualMachine_SJB/CS6510_VirtualMachine_SJB/AssemblyInstructionEnum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS6510_VirtualMachine_SJB
+{
+ public enum AssemblyInstruction
+    {
+        ADD = 16,
+        SUB = 17,
+        MUL = 18,
+        DIV = 19,
+        MOV = 1,
+        MVI = 22,
+        ADR = 0,
+        STR = 2,
+        STRB = 3,
+        LDR = 4,
+        LDRB = 5,
+        B = 7,
+        BX = 6,
+        BNE = 8,
+        BGT = 9,
+        BLT = 10,
+        BEQ = 11, 
+        CMP = 12,
+        AND = 13,
+        ORR = 14,
+        EOR = 15, 
+        SWI = 20,
+    }
+}
---
+++ CS6510_VirtualMachine_SJB/CS6510_VirtualMachine_SJB/VirtualMachine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CS6510_VirtualMachine_SJB
+{
+    class VirtualMachine
+    {
+        public SortedDictionary<string, int> REG = new SortedDictionary<string, int>();
+        public  byte[] MEM = new byte[10000];
+        public int PC = 0;
+        public int loaderAddress = 0;
+        public VirtualMachine()
+        {
+            REG.Add("R0", 0);
+            REG.Add("R1", 0);
+            REG.Add("R2", 0);
+            REG.Add("R3", 0);
+            REG.Add("R4", 0);
+            REG.Add("R5", 0);
+            REG.Add("SP", 0);
+            REG.Add("FP", 0);
+            REG.Add("SL", 0);
+            REG.Add("Z", 0);
+        }
+
+
+    }
+}
