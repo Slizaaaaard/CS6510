182ba22da1ca0fb9684a0267526b075e855c7460
Slizaaaaard <sjbush92@gmail.com>
2019-08-27 13:26:07 -0600

    yase enter the commit message fh '#' will be ignored, and an empty message aborts the commit.  On branch Milstone0 ified:   CS6510_VirtualMachine_SJB/CS6510_VirtualMachine_SJB/Program.cs

--- CS6510_VirtualMachine_SJB/CS6510_VirtualMachine_SJB/Program.cs
+++ CS6510_VirtualMachine_SJB/CS6510_VirtualMachine_SJB/Program.cs
@@ -1,12 +1,100 @@
 ﻿using System;
-
+using System.IO;
+using System.Text;
 namespace CS6510_VirtualMachine_SJB
 {
     class Program
     {
+        static VirtualMachine VM = new VirtualMachine();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            do
+            {
+                loadProgram();
+                executeProgram();
+                Console.WriteLine("\n");
+            } while (Console.ReadLine() != "s");
+            Console.ReadKey();
+        }
+     
+        static void loadProgram()
+        {
+            using (FileStream fs = new FileStream("cpuBound.osx", FileMode.Open, FileAccess.Read))
+            {
+                using (BinaryReader br = new BinaryReader(fs, new ASCIIEncoding()))
+                {
+
+                    int bSize = br.ReadInt32();
+                    VM.PC = br.ReadInt32();
+                    VM.loaderAddress = br.ReadInt32();
+                    int fetchAddr = VM.loaderAddress;
+                    int dataAddr = VM.loaderAddress;
+                    int datAddr = VM.loaderAddress;
+                    Console.WriteLine(bSize);
+                    Console.WriteLine(VM.PC);
+                    Console.WriteLine(VM.loaderAddress);
+
+                    for (int i = 0; i < VM.PC; i++)
+                    {
+                        VM.MEM[i] = br.ReadByte();
+                        Console.WriteLine(VM.MEM[i]);
+                    }
+                    Console.WriteLine("end");
+                    for (int i = VM.PC; i < bSize; i += 6)
+                    {
+                        byte Byte = br.ReadByte();
+                        Console.WriteLine(Byte);
+                        switch (Byte)
+                        {
+                            case (byte)AssemblyInstruction.ADD:
+                                Console.WriteLine("\nAdd");
+                                VM.MEM[VM.loaderAddress++] = Byte;
+                                VM.MEM[VM.loaderAddress++] = br.ReadByte();
+                                VM.MEM[VM.loaderAddress++] = br.ReadByte();
+                                VM.MEM[VM.loaderAddress++] = br.ReadByte();
+                                br.ReadByte();
+                                br.ReadByte();
+                                break;
+                            case (byte)AssemblyInstruction.SUB:
+                                Console.WriteLine("\nSubtract");
+                                VM.MEM[VM.loaderAddress++] = Byte;
+                                VM.MEM[VM.loaderAddress++] = br.ReadByte();
+                                VM.MEM[VM.loaderAddress++] = br.ReadByte();
+                                VM.MEM[VM.loaderAddress++] = br.ReadByte();
+                                br.ReadByte();
+                                br.ReadByte();
+                                break;
+                            case (byte)AssemblyInstruction.MUL:
+                                Console.WriteLine("\nMultiply");
+                                VM.MEM[VM.loaderAddress++] = Byte;
+                                VM.MEM[VM.loaderAddress++] = br.ReadByte();
+                                VM.MEM[VM.loaderAddress++] = br.ReadByte();
+                                VM.MEM[VM.loaderAddress++] = br.ReadByte();
+                                br.ReadByte();
+                                br.ReadByte();
+                                break;
+                            case (byte)AssemblyInstruction.DIV:
+                                Console.WriteLine("\nDivide");
+                                VM.MEM[VM.loaderAddress++] = Byte;
+                                VM.MEM[VM.loaderAddress++] = br.ReadByte();
+                                VM.MEM[VM.loaderAddress++] = br.ReadByte();
+                                VM.MEM[VM.loaderAddress++] = br.ReadByte();
+                                br.ReadByte();
+                                br.ReadByte();
+                                break;
+                        }
+
+                    }
+                }
+            }
+        }
+        static void executeProgram()
+        {
+
         }
     }
 }
+
+
+
